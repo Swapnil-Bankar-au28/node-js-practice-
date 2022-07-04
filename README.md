b0312bf41f73ee5c713c16be543f45a75794ea92
Swapnil-Bankar-au28 <sbankar217@gmail.com>
2022-07-04 13:23:50 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-ddefwy
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-ddefwy)
---
+++ index.js
@@ -0,0 +1,10 @@
+// run `node index.js` in the terminal
+let name = {
+  fname = "swapnil",
+  lname = "bankar ",
+
+  printName: function (){
+    console.log(this.fname  + " " + this.lname)
+  }
+}
+name.printName();
